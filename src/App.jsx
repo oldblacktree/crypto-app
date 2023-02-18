8f91aa4a59eb385944111424db8414d9cdf5ebbb
Danil <oldblacktree@gmail.com>
2023-02-18 19:53:38 +0300

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,20 @@ const App = () => {
   return (
     <div className="app">
       <Navbar />
-      <main className="main">
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
-        <Route path="/exchanges" element={<Exchanges />} />
-        <Route path="/crypto/:coinId" element={<CryptoDetails />} />
-        <Route path="/news" element={<News />} />
-      </Routes>
+      <main className="app__main">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+          <Route path="/exchanges" element={<Exchanges />} />
+          <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+          <Route path="/news" element={<News />} />
+        </Routes>
+        <footer className="app__footer">
+          <p className="app__footer-info">
+            Copyright © 2023 Cryptoverse Inc. <br />
+            Not All Rights Reserved
+          </p>
+      </footer>
       </main>
     </div>
   );
