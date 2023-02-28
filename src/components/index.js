5f0feebcb89b1f2a710332cf6d6423317ac7fa96
Danil <oldblacktree@gmail.com>
2023-02-28 23:32:08 +0300

    feat: add loader

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import CryptoDetails from "./CryptoDetails/CryptoDetails";
 import Exchanges from "./Exchanges/Exchanges";
 import Homepage from "./Homepage/Homepage";
 import News from "./News/News";
+import Loader from "./Loader/Loader";
 
-export { Navbar, Cryptocurrencies, CryptoDetails, Exchanges, Homepage, News };
+export { Navbar, Cryptocurrencies, CryptoDetails, Exchanges, Homepage, News, Loader };
